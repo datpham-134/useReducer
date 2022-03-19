1bc1a64ef42644de1aa8d2b3c8ddc63660d6ca47
dat <datpham123335@gmail.com>
2022-03-19 17:27:31 +0700

    create todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'add-todo':
+      return {
+        todos: [...state.todos, { text: action.payload, completed: false }],
+        todoCount: state.todoCount + 1,
+      };
+    case 'toggle-todo':
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.payload ? { ...t, completed: !t.completed } : t
+        ),
+        todoCount: state.todoCount,
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0,
+  });
+  const [text, setText] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: 'add-todo', payload: text });
+          setText('');
+        }}
+      >
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </form>
+      <div>number of todos: {todoCount}</div>
+      {todos.map((t, idx) => (
+        <div
+          key={t.text}
+          onClick={() => dispatch({ type: 'toggle-todo', payload: idx })}
+          style={{ textDecoration: t.completed ? 'line-through' : '' }}
         >
-          Learn React
-        </a>
-      </header>
+          {t.text}
+        </div>
+      ))}
+      {/* <pre>{JSON.stringify(todos, null, 2)}</pre> */}
     </div>
   );
 }
